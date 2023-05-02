b104c6d3714dc52b963d5314087c6a25d1d686da
Borhan <brborhan70@gmail.com>
2023-05-03 00:13:12 +0600

    register section complate and firebase store success

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId:import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyC1L2IN0O0dUB_DTsvGVGtu5SIeZMcqJJk",
+  authDomain: "chef-hunter-a4419.firebaseapp.com",
+  projectId: "chef-hunter-a4419",
+  storageBucket: "chef-hunter-a4419.appspot.com",
+  messagingSenderId: "554998421713",
+  appId: "1:554998421713:web:bf775c854930f6a73f8df0"
 };
 
 // Initialize Firebase
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,37 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
+
+
+
+
+
 
 const Register = () => {
+    // eslint-disable-next-line no-unused-vars
+    const {createUser} = useContext(AuthContext)
+
+    const [email,setEmail] = useState("")
+    const [password,setPassword]= useState("")
+    const [name,setName] = useState("")
+    const [photo, setPhoto] = useState("")
+
+    const handleRegister = event =>{
+        event.preventDefault();
+        if((name, email,password, photo)){
+            createUser(email,password)
+            .then((result) => {
+                console.log(result.user);
+              })
+              .catch((err) => {
+                console.log(err.message);
+              });
+          }
+
+    }
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -10,37 +39,37 @@ const Register = () => {
     <div className="text-center ">
       <h1 className="text-5xl font-bold text-transparent bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text">SingUp now!</h1>
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100" onSubmit={handleRegister}>
       <div className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="test" placeholder="Name" name='name' className="input input-bordered" />
+          <input type="test" placeholder="Name" name='name' className="input input-bordered" onChange={(e) => setName(e.target.value)} required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" name='email' className="input input-bordered" />
+          <input type="text" placeholder="email" name='email' className="input input-bordered" onChange={(e) => setEmail(e.target.value)} required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Photo URL</span>
           </label>
-          <input type="text" placeholder="Photo URL" name='photo' className="input input-bordered" />
+          <input type="text" placeholder="Photo URL" name='photo' className="input input-bordered" onChange={(e) => setPhoto(e.target.value)} required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" name='password' className="input input-bordered" />
+          <input type="password" placeholder="password" name='password' className="input input-bordered" onChange={(e) => setPassword(e.target.value)} required />
           <label className="label">
           <p> Already Have an Account? <Link to="/login" className='text-blue-700 underline'>Login</Link></p>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary bg-gradient-to-r from-indigo-600 to-purple-600 outline-none bottom-0">Login</button>
+          <button className="btn btn-primary bg-gradient-to-r from-indigo-600 to-purple-600 outline-none bottom-0" type='submit' onClick={handleRegister}>Login</button>
         </div>
       </div>
     </div>
