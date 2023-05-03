42c69eeb948c214e465aa6a10fbe7b1ab37b4df3
Borhan <brborhan70@gmail.com>
2023-05-04 05:38:50 +0600

    banner section add complate

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,15 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <p>this is banner</p>
+         <div className='flex my-8 justify-around font-serif  mx-10 items-center'>
+            <div className='ml-16 basis-1/2'>
+                 <h1  className=' text-transparent bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-5xl font-bold italic'>Welcome to <br />  Chef Hunter </h1>
+                 <p className='p-6'>A Chef or Cook is responsible for using their culinary expertise to create appetizing dishes for diners to enjoy. Their duties include overseeing kitchen staff, tasting dishes before going to customers and restocking food produce as needed</p>
+            </div>
+            <div className='basis-1/2'>
+                <img src="https://d2gg9evh47fn9z.cloudfront.net/1600px_COLOURBOX5231682.jpg" className='w-1/2 ml-32' alt="" />
+            </div>
+         </div>
         </div>
     );
 };
