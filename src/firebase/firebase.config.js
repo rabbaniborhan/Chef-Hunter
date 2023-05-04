415c6ebbc7bf792efba0e52fa3152295cd865ccb
Borhan <brborhan70@gmail.com>
2023-05-04 23:51:49 +0600

    env setup done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log("firebaseconfig",import.meta.env.VITE_APPID);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1L2IN0O0dUB_DTsvGVGtu5SIeZMcqJJk",
-  authDomain: "chef-hunter-a4419.firebaseapp.com",
-  projectId: "chef-hunter-a4419",
-  storageBucket: "chef-hunter-a4419.appspot.com",
-  messagingSenderId: "554998421713",
-  appId: "1:554998421713:web:bf775c854930f6a73f8df0"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
